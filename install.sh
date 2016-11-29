2a61bb7632f802a363607bdde5868a70d2a679f8
iwamoto yuma <yuma@yumac.local>
2016-11-29 21:25:50 +0900

    fixed files

--- install.sh
+++ install.sh
@@ -6,14 +6,15 @@ sudo apt install git gcc sudo sqlite3 make apache2 -y
 wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
 
-echo "export GOROOT=/usr/local/go/" >>/home/yuma/.bashrc
-echo "export GOPATH=$HOME/go" >> /home/yuma/.bashrc
-echo "export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> /home/yuma/.bashrc
+sudo echo "export GOROOT=/usr/local/go/" >>/home/yuma/.bashrc
+sudo echo "export GOPATH=$HOME/go" >> /home/yuma/.bashrc
+sudo echo "export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> /home/yuma/.bashrc
 #install vuls
 sudo mkdir /var/log/vuls
 sudo chown -R yuma  /var/log/vuls
 sudo chmod 700 /var/log/vuls
-mkdir -p /usr/local/go/src/github.com/cve-dict
+
+sudo mkdir -p /usr/local/go/src/github.com/cve-dict
 cd /usr/local/go/src/github.com/cve-dict
 git clone https://github.com/kotakanbe/go-cve-dictionary.git
 cd go-cve-dictionary
@@ -25,7 +26,7 @@ sudo chown -R yuma /opt/vuls
 chmod 777 /opt/vuls
 for i in {2002..2016}; do go-cve-dictionary fetchnvd -years $i; done
 ls -alh cve.sqlite3
-mkdir -p $GOPATH/src/github.com/future-architect
+sudo mkdir -p $GOPATH/src/github.com/future-architect
 cd $GOPATH/src/github.com/future-architect
 git clone https://github.com/future-architect/vuls.git
 cd vuls
